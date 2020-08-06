3e10e1e7587ff42ebdbeb41aba790504a559e209
Garbiñe <gboned@cifpfbmoll.eu>
2020-08-06 16:16:59 +0200

    feat(main)
    Añadir el main del proyecto

---
+++ main.py
@@ -0,0 +1,38 @@
+def main():
+
+    # Preparamos las taquillas
+    locker1 = Locker(
+        '1', # identificador
+        'CLOSED', # si está abierta o cerrada 
+        False, # si está ocupado
+        '' # código del paquete que lo ocupa (no hay)
+    )
+    locker2 = Locker(
+        '1', # identificador
+        'CLOSED', # si está abierta o cerrada
+        False, # si está ocupado
+        '' # código del paquete que lo ocupa (no hay)
+    )
+
+    # Creamos la instancia del punto de recogida
+    lockerPoint = LockerPoint(
+        'sr6cxt7v8fybw9n', # identificador
+        'Calle de las Flores, 22', # dirección
+        [locker1, locker2] # lista de taquillas 
+    )
+
+    # Creamos un paquete para dejar...
+    package1 = Package(
+        'ldvbsbdd', # código identificador
+        'NOT REGISTERED', # estado
+    )
+    # y otro para recoger
+    package2 = Package(
+        'ldvbsbdd', # código identificador
+        'KEEPING', # estado
+    )
+
+
+
+if __name__ == '__main__':
+    main()
