54378eb803c448c21e649f6ade078e245d8d55f0
Garbiñe <gboned@cifpfbmoll.eu>
2020-08-08 12:26:09 +0200

    style(main): Descomentar código

--- app/main.py
+++ app/main.py
@@ -12,29 +12,29 @@ def main():
         '' # código del paquete que lo ocupa (no hay)
     )
     locker2 = Locker(
-        '1', # identificador
+        '2', # identificador
         'CLOSED', # si está abierta o cerrada
         False, # si está ocupado
         '' # código del paquete que lo ocupa (no hay)
     )
 
-    # # Creamos la instancia del punto de recogida
-    # lockerPoint = LockerPoint(
-    #     'sr6cxt7v8fybw9n', # identificador
-    #     'Calle de las Flores, 22', # dirección
-    #     [locker1, locker2] # lista de taquillas 
-    # )
+    # Creamos la instancia del punto de recogida
+    lockerPoint = LockerPoint(
+        'sr6cxt7v8fybw9n', # identificador
+        'Calle de las Flores, 22', # dirección
+        [locker1, locker2] # lista de taquillas 
+    )
 
-    # # Creamos un paquete para dejar...
-    # package1 = Package(
-    #     'PACK-111-222-333', # código identificador
-    #     'NOT REGISTERED', # estado
-    # )
-    # # y otro para recoger
-    # package2 = Package(
-    #     'PACK-165-997-232', # código identificador
-    #     'KEEPING', # estado
-    # )
+    # Creamos un paquete para dejar...
+    package1 = Package(
+        'PACK-111-222-333', # código identificador
+        'NOT REGISTERED', # estado
+    )
+    # y otro para recoger
+    package2 = Package(
+        'PACK-165-997-232', # código identificador
+        'KEEPING', # estado
+    )
 
 
 
